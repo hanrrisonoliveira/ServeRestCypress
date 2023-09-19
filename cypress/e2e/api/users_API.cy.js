9d2e3dfa5bc9de2f33912d217c5e755668983f27
Hanrrison Oliveira <hanrrison.dos@gmail.com>
2023-09-19 11:35:15 -0300

    Users test

---
+++ cypress/e2e/api/users_API.cy.js
@@ -0,0 +1,64 @@
+const API_URL = Cypress.env('API_URL')
+
+describe('Users test', () => {
+    it('Search for all registered users', () => {
+        cy.api({
+            method: 'GET',
+            url: `${API_URL}/usuarios`
+        }).then(({ status, body }) => {
+            expect(status).to.equal(200)
+            expect(body.usuarios.length)
+
+            var arraylength = body.usuarios.length
+            cy.log('Array Length', arraylength)
+            expect(body.usuarios.length).to.be.eq(arraylength)
+
+            for (let i = 0; i < arraylength; i++) {
+                expect(body.usuarios[i].administrador)
+                cy.log('Name: ' + body.usuarios[i].nome + ',' + ' Email: ' + body.usuarios[i].email)
+            }
+        })
+    })
+
+    it('Administrator users', () => {
+        cy.api({
+            method: 'GET',
+            url: `${API_URL}/usuarios`
+        }).then(({ status, body }) => {
+            expect(status).to.equal(200)
+            expect(body.usuarios.length)
+
+            var arraylength = body.usuarios.length
+            cy.log('Array Length', arraylength)
+            expect(body.usuarios.length).to.be.eq(arraylength)
+
+            for (let i = 0; i < arraylength; i++) {
+                expect(body.usuarios[i].administrador)
+                if (body.usuarios[i].administrador === 'true') {
+                    cy.log('Name: ' + body.usuarios[i].nome + ',' + ' Email: ' + body.usuarios[i].email)
+                }
+            }
+        })
+    })
+
+    it('Non administrator users ', () => {
+        cy.api({
+            method: 'GET',
+            url: `${API_URL}/usuarios`
+        }).then(({ status, body }) => {
+            expect(status).to.equal(200)
+            expect(body.usuarios.length)
+
+            var arraylength = body.usuarios.length
+            cy.log('Array Length', arraylength)
+            expect(body.usuarios.length).to.be.eq(arraylength)
+
+            for (let i = 0; i < arraylength; i++) {
+                expect(body.usuarios[i].administrador)
+                if (body.usuarios[i].administrador === 'false') {
+                    cy.log('Name: ' + body.usuarios[i].nome + ',' + ' Email: ' + body.usuarios[i].email)
+                }
+            }
+        })
+    })
+})
